5c65a2de5f56c34e1d828270742c309e6fd4ec08
xavierxav <73179616+xavierxav@users.noreply.github.com>
2021-01-29 12:19:36 +0100

    Update initialisation_etage.cpp

--- src/initialisation_etage.cpp
+++ src/initialisation_etage.cpp
@@ -1,20 +1,27 @@
 #include <ncurses.h>
-
+#
+constexpr int murcote=1;
+constexpr int murbas=2;
+constexpr int sol=3;
+constexpr int hero=4;
+constexpr int armure=5;
+constexpr int arme=6;
+constexpr int potion;
 
 void initialisation_etage(int* l,WINDOW * win){
     wmove(win, 0, 0);
     for(int i=0;i<30*60;i++){
-        if(l[i]==0){
+        if(l[i]==murcote){
             waddch(win, '|');
         }
-        if(l[i]==0){
-            waddch(win, '|');
+        if(l[i]==murbas){
+            waddch(win, '-');
         }
-        if(l[i]==0){
-            waddch(win, '|');
+        if(l[i]==sol){
+            waddch(win, '.');
         }
-        if(l[i]==0){
-            waddch(win, '|');
+        if(l[i]==hero){
+            waddch(win, '@');
         }
         if(l[i]==0){
             waddch(win, '|');
