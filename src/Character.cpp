47beeb7b49f2c5199ce609d06c1a5038b3ce435e
Caroline Jeandat <caroline.jeandat@mines-paristech.fr>
2021-01-29 13:08:25 +0100

    classe character : utilisation d'objets

--- src/Character.cpp
+++ src/Character.cpp
@@ -4,26 +4,58 @@
 Character::Character ( int x, int y, int etage ) : 
     position( { x, y, etage } ), pdv(100), strength(5), defense(1), gold(0) {}
 
-int* Character::get_position () { return position; }
+
+int* Character::get_position () const { return position; }
 void Character::move ( int axis, int direction ) { position[axis] += direction; }
+
 
 void Character::incr_life () { pdv += 1; }
 void Character::decr_life () { pdv -= 10 - defense; }
-int Character::get_life () { return pdv; }
+int Character::get_life () const { return pdv; }
+
 
 void Character::incr_gold ( int value ) { gold += value; }
 void Character::decr_gold ( int value ) { gold -= value; }
-int Character::get_gold () { return gold; }
+int Character::get_gold () const { return gold; }
+
 
 void Character::incr_strength ( int value ) { strength += value; }
 void Character::decr_strength ( int value ) { strength -= value; }
-int Character::get_strength () { return strength; }
+int Character::get_strength () const { return strength; }
+
 
 void Character::incr_defense ( int value ) { defense += value; }
 void Character::decr_defense ( int value ) { defense -= value; }
-int Character::get_defense () { return defense; }
+int Character::get_defense () const { return defense; }
 
-void Character::get_object ( Object* object )
+
+std::string Character::add_object ( Object* object )
 {
-    g
+    if ( besace.size() < size_max )
+    {
+        besace.pushback( object );
+        object->equip( this );
+        return "objet ajouté à la besace"
+    }
+    else
+    {
+        return "la besace est pleine"
+    }
+}
+void Character::remove_object ( int index ) 
+{
+    besace[index]->ditch( this );
+    besace.erase( index );
+}
+std::string Character::print_besace () const
+{
+    std::string list_objects = "";
+    for ( int i=0 ; i<size_max ; i++ )
+    {
+        list_objects += to_string(i);
+        list_objects += ". ";
+        list_objects += besace[i]->print();
+        list_objects += "\n";
+    }
+    return list_objects;
 }
