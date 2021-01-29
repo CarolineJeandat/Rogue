4f8010f15fa73144b8ff7b91402406fd1ebf3c41
eliselei <elise.yuzhi.lei@gmail.com>
2021-01-29 14:39:58 +0100

    début de potion

--- include/Object.h
+++ include/Object.h
@@ -10,11 +10,12 @@ class Object {
         std::string type;
         std::string description;
     public :
+        std::string getType() const ;
         virtual void equip(Character* perso) = 0;
         virtual void use(Character* perso) = 0;
         virtual void unuse(Character* perso) = 0;
         virtual void ditch(Character* perso) const = 0;
-        virtual std::string print() = 0;
+        virtual std::string printObject() = 0;
 };
 
 class Arme : public Object {
@@ -48,6 +49,17 @@ class Food : public Object {
         int energy;
     public :
         Food(int energy);
+        void use(Character* perso);
+        void equip(Character* perso);
+        void unuse(Character* perso);
+        void ditch(Character* perso);
+};
+
+class Potion : public Object {
+    private :
+        int effect;
+    public :
+        Potion(int effect);
         void use(Character* perso);
         void equip(Character* perso);
         void unuse(Character* perso);
