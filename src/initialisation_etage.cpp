219f10bc67fb1b032725a6969f7ed4751b1fa6e8
xavierxav <73179616+xavierxav@users.noreply.github.com>
2021-01-29 12:10:26 +0100

    Update initialisation_etage.cpp

--- src/initialisation_etage.cpp
+++ src/initialisation_etage.cpp
@@ -1,3 +1,24 @@
-void initialisation_etage(int* l){
-    
+#include <ncurses.h>
+
+
+void initialisation_etage(int* l,WINDOW * win){
+    wmove(win, 0, 0);
+    for(int i=0;i<30*60;i++){
+        if(l[i]==0){
+            waddch(win, '|');
+        }
+        if(l[i]==0){
+            waddch(win, '|');
+        }
+        if(l[i]==0){
+            waddch(win, '|');
+        }
+        if(l[i]==0){
+            waddch(win, '|');
+        }
+        if(l[i]==0){
+            waddch(win, '|');
+        }
+    }
+
 }
