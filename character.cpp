c1725620f015c0e4f5c0d916e09d6e5d581ce42f
Caroline Jeandat <caroline.jeandat@mines-paristech.fr>
2021-01-29 10:16:49 +0100

    qq trucs dans characeter

--- character.cpp
+++ character.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <>
 
 /* 
 point de vie, besace : objets + argent, force, (état de santé : si les objets sont toxiques)
@@ -11,7 +12,12 @@ class Character
         void gagne_vie () { pdv += 1; }
         void perd_vie () { pdv -= 1 }
 
+        void print_vie () { std::cout << "il vous reste " << pdv << "points de vie !" << std::endl; }
+
     private :
         int pdv = 10;
+        int force = 5;
+        besace
+
 
 };
